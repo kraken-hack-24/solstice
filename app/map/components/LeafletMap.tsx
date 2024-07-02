247e17854277129cfd8445013d2090f64e1bdae3
614912164 <donovan.hare@bt.com>
2024-07-02 13:48:42 +0100

    Remove defunct map code

--- app/map/components/LeafletMap.tsx
+++ app/map/components/LeafletMap.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 import "leaflet-defaulticon-compatibility"
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
@@ -15,19 +15,13 @@ import 'next-leaflet-cluster/lib/assets/marker-shadow.png'
 import { Property } from '../../models/property.model';
 import { useMapContext } from '@/contexts/mapdata/useMapContext';
 import PropertyMarker from './PropertyMarker';
-import { useCallback, useEffect, useState } from 'react';
-import { Map } from 'leaflet';
-
 
 
 const LeafletMap = () => {
-    const { data, map, setMap } = useMapContext();
+    const { data, setMap } = useMapContext();
 
     return (
         <>
-            {map ? <DisplayPosition map={map} /> : null}
-
-            
             <MapContainer ref={setMap} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} className='rounded-lg'>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -46,35 +40,6 @@ const LeafletMap = () => {
             </MapContainer>
         </>
     );
-}
-
-const center = [51.505, -0.09]
-const zoom = 13
-
-function DisplayPosition({ map }: { map: Map }) {
-    const [position, setPosition] = useState(() => map.getCenter())
-
-    const onClick = useCallback(() => {
-        map.setView(center, zoom)
-    }, [map])
-
-    const onMove = useCallback(() => {
-        setPosition(map.getCenter())
-    }, [map])
-
-    useEffect(() => {
-        map.on('move', onMove)
-        return () => {
-            map.off('move', onMove)
-        }
-    }, [map, onMove])
-
-    return (
-        <p>
-            latitude: {position.lat.toFixed(4)}, longitude: {position.lng.toFixed(4)}{' '}
-            <button onClick={onClick}>reset</button>
-        </p>
-    )
 }
 
 export default LeafletMap
