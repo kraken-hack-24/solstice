d2bdf0890332f32934c7796fe12c3352113fe1eb
614912164 <donovan.hare@bt.com>
2024-07-02 11:16:29 +0100

    Add property side card

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import {
   MapIcon
 } from '@heroicons/react/24/outline'
+import PropertySideCard from "./components/PropertySideCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,53 +24,65 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    
+
       <body className={inter.className}>
-      <>
-      <div>
-        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-          
-          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4">
-            <div className="flex h-16 shrink-0 items-center">
-              Solstice
-            </div>
-            <nav className="flex flex-1 flex-col">
-              <ul role="list" className="flex flex-1 flex-col gap-y-7">
-                <li>
-                  <ul role="list" className="-mx-2 space-y-1">
-                    {navigation.map((item) => (
-                      <li key={item.name}>
-                        <a
-                          href={item.href}
-                          className={'bg-gray-800 text-white group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6'}
-                        >
-                          <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
+        <>
+          <div>
+            <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+
+              <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4">
+                <div className="flex h-16 shrink-0 items-center">
+                  Solstice
+                </div>
+                <nav className="flex flex-1 flex-col">
+                  <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                    <li>
+                      <ul role="list" className="-mx-2 space-y-1">
+                        {navigation.map((item) => (
+                          <li key={item.name}>
+                            <a
+                              href={item.href}
+                              className={'bg-gray-800 text-white group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6'}
+                            >
+                              <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
+                              {item.name}
+                            </a>
+                          </li>
+                        ))}
+                      </ul>
+                    </li>
+
                   </ul>
-                </li>
-                
-              </ul>
-            </nav>
-          </div>
-        </div>
 
-        <div className="lg:pl-72">
-          
 
-          <main className="py-10">
-            <div className="px-4 sm:px-6 lg:px-8">
-            {children}
+                  <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                    <li>
+                      <ul role="list" className="-mx-2 space-y-1">
+
+                        <PropertySideCard />
+                        <PropertySideCard />
+                        <PropertySideCard />
+                      </ul>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
             </div>
-          </main>
-        </div>
-      </div>
-    </>
-        
-        
-        
+
+            <div className="lg:pl-72">
+
+
+              <main className="py-10">
+                <div className="px-4 sm:px-6 lg:px-8">
+                  {children}
+                </div>
+              </main>
+            </div>
+          </div>
+        </>
+
+
+
       </body>
     </html>
   );
